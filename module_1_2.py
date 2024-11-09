51b7d99c4ce9c1895b534d800c04771f2b985649
s1984fps <s1984fps@gmail.com>
2024-11-09 02:28:25 -0500

    Create module_1_2.py

---
+++ module_1_2.py
@@ -0,0 +1,4 @@
+homeworks_amount = 12
+spend_hours = 1.5
+course = 'Python'
+print("Курс:", course, "всего задач:", homeworks_amount,"затрачено часов:",spend_hours,"среднее время выполнения",spend_hours/homeworks_amount)
