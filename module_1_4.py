8a1eff86d25a47067dec8677041df106dc64393f
s1984fps <s1984fps@gmail.com>
2024-11-17 14:04:02 -0500

    Create module_1_4.py

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string = input('Введите текст')
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+my_string = my_string.replace(' ','')
+print(my_string)
+print(my_string[0])
+print(my_string[-1])
